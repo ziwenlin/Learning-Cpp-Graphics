e109f2621f6d60f59878a92327b1b57265dd9e51
ziwenlin <ziwen_lin@outlook.com>
2025-02-25 15:35:13 +0100

    Added: testen voor methode setTimeStep voor PhysicsObject

--- tests/test_PhysicsObject.cc
+++ tests/test_PhysicsObject.cc
@@ -5,18 +5,49 @@
 
 TEST(PhysicsObject, UpdateGetPosition) {
     PhysicsObject physics_object(sf::Vector2f(300.f, 300.f));
+    physics_object.setTimeStep(1.0f / 2.0f);
     std::string position = toString(physics_object.getPosition());
     EXPECT_EQ(position, "300.0 300.0");
 
     physics_object.applyForce(sf::Vector2f(10.f, 0.f));
-    physics_object.applyMovement(.5f);
+    physics_object.applyMovement();
     position = toString(physics_object.getPosition());
     EXPECT_EQ(position, "302.5 300.0");
 
     physics_object.applyForce(sf::Vector2f(10.f, 0.f));
-    physics_object.applyMovement(.5f);
+    physics_object.applyMovement();
     position = toString(physics_object.getPosition());
     EXPECT_EQ(position, "307.5 300.0");
+}
+
+TEST(PhysicsObject, CorrectConversionSetTimeStep) {
+    PhysicsObject physics_object(sf::Vector2f(300.f, 300.f));
+    std::string position = toString(physics_object.getPosition());
+    EXPECT_EQ(position, "300.0 300.0");
+
+    physics_object.setTimeStep(1.0f / 2.0f);
+    const float time_step = physics_object.getTimeStep();
+    EXPECT_FLOAT_EQ(time_step, 1.0f / 2.0f);
+
+    position = toString(physics_object.getPosition());
+    EXPECT_EQ(position, "300.0 300.0");
+
+    physics_object.applyForce(sf::Vector2f(10.f, 0.f));
+    physics_object.applyMovement();
+    position = toString(physics_object.getPosition());
+    EXPECT_EQ(position, "302.5 300.0");
+
+    physics_object.setTimeStep(1.0f);
+    physics_object.applyForce(sf::Vector2f(10.f, 0.f));
+    physics_object.applyMovement();
+    position = toString(physics_object.getPosition());
+    EXPECT_EQ(position, "317.5 300.0");
+
+    physics_object.setTimeStep(1.0f / 10.0f);
+    physics_object.applyForce(sf::Vector2f(10.f, 0.f));
+    physics_object.applyMovement();
+    position = toString(physics_object.getPosition());
+    EXPECT_EQ(position, "319.1 300.0");
 }
 
 TEST(PhysicsObject, InitializeShapeLength) {
