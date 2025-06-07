14e76798462ce591661f8f8e4bfdb7308507b524
ziwenlin <ziwen_lin@outlook.com>
2025-06-07 16:53:47 +0200

    Added: class DevInfoWindow

---
+++ include/DevInfoWindow.h
@@ -0,0 +1,41 @@
+#ifndef DEVELOPMENTWINDOW_H
+#define DEVELOPMENTWINDOW_H
+
+#include <SFML/Graphics.hpp>
+
+#include "SmartKeyboard.h"
+
+
+class DevInfoWindow {
+public:
+
+private:
+    sf::RenderWindow window;
+    sf::Font font;
+    std::vector<sf::Text> texts;
+
+    bool is_active = false;
+    bool is_hidden = true;
+
+    SmartKeyboard keyboard;
+    const int key_close = keyboard.addKey(sf::Keyboard::Key::P);
+
+public:
+    DevInfoWindow();
+
+    ~DevInfoWindow();
+
+    bool update();
+
+    bool draw();
+
+    bool toggle();
+
+    bool enable();
+
+    bool disable();
+
+private:
+};
+
+#endif //DEVELOPMENTWINDOW_H
---
+++ src/DevInfoWindow.cpp
@@ -0,0 +1,81 @@
+#include "DevInfoWindow.h"
+
+#include "fmt/format.h"
+
+DevInfoWindow::DevInfoWindow() {
+    const bool font_success = this->font.openFromFile(R"(C:\Windows\Fonts\Verdana.ttf)");
+    if (font_success == false) {
+        fmt::println("Failed to load font file!");
+    }
+    window.create(sf::VideoMode(sf::Vector2u(600u, 800u)), "Info", sf::Style::Close);
+    window.setPosition(sf::Vector2i(-1000, -1000));
+    window.setFramerateLimit(0);
+    window.setVisible(false);
+}
+
+DevInfoWindow::~DevInfoWindow() {
+    window.close();
+}
+
+bool DevInfoWindow::update() {
+    if (is_active == false) {
+        return false;
+    }
+    if (this->window.isOpen()) {
+        while (std::optional<sf::Event> event = this->window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                this->disable();
+            }
+        }
+        this->keyboard.update(this->window.hasFocus());
+        if (keyboard.getKey(key_close).isPressedUp() == true) {
+            this->disable();
+        }
+    }
+
+
+    return true;
+}
+
+bool DevInfoWindow::draw() {
+    if (is_active == false) {
+        return false;
+    }
+    if (is_hidden == true) {
+        window.setPosition(sf::Vector2i(10, 10));
+        is_hidden = false;
+    }
+    window.clear(sf::Color::Black);
+    for (const auto &text: this->texts) {
+        window.draw(text);
+    }
+    window.display();
+    return true;
+}
+
+bool DevInfoWindow::toggle() {
+    if (is_active == false) {
+        enable();
+        return false;
+    }
+    disable();
+    return true;
+}
+
+bool DevInfoWindow::enable() {
+    is_active = true;
+    // window.setPosition(sf::Vector2i(-1000, -1000));
+    window.clear(sf::Color::Black);
+    window.setVisible(true);
+    window.display();
+    return true;
+}
+
+bool DevInfoWindow::disable() {
+    is_active = false;
+    is_hidden = true;
+    window.setPosition(sf::Vector2i(-1000, -1000));
+    window.setVisible(false);
+    window.display();
+    return true;
+}
