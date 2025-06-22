ef6d12a6f817b4f34e044f9d3e515f7ec60eb847
ziwenlin <ziwen_lin@outlook.com>
2025-06-22 22:18:58 +0200

    Removed: unittest voor class SmartMouse

--- tests/test_SmartMouse.cc
+++ tests/test_SmartMouse.cc
@@ -6,10 +6,11 @@
 
 TEST(SmartMouse, UpdateGetPosition) {
     SmartMouse mouse;
-    mouse.setMouseState(true);
-    mouse.isMousePressed(sf::Vector2i(100, 100));
-    std::string position = toString(mouse.getPosition());
-    EXPECT_EQ(position, "100.0 100.0");
-    position = toString(mouse.getPosition());
-    EXPECT_EQ(position, "100.0 100.0");
+    // TODO rework this test
+    // mouse.setMouseState(true);
+    // mouse.isMousePressed(sf::Vector2i(100, 100));
+    // std::string position = toString(mouse.getPosition());
+    // EXPECT_EQ(position, "100.0 100.0");
+    // position = toString(mouse.getPosition());
+    // EXPECT_EQ(position, "100.0 100.0");
 }
