8ae29d616be99b7672dbdad4920c25f04e9864f3
ziwenlin <ziwen_lin@outlook.com>
2026-02-08 20:00:14 +0100

    Added afhandelen van methode load in class ScreenContentManager

--- src/devices/ScreenContentManager.cpp
+++ src/devices/ScreenContentManager.cpp
@@ -1,5 +1,6 @@
 #include "ScreenContentManager.h"
 
+#include <iostream>
 #include <nlohmann/json.hpp>
 
 void ScreenContentManager::setPath(const char *path) {
@@ -7,19 +8,41 @@ void ScreenContentManager::setPath(const char *path) {
 }
 
 void ScreenContentManager::load() {
-    std::string basic_string;
-    if (!m_file.read(basic_string)) {
+    nlohmann::json json;
+    if (std::string basic_string; !m_file.read(basic_string)) {
         return;
+    } else {
+        json = nlohmann::json::parse(basic_string);
     }
-    nlohmann::json json = nlohmann::json::parse(basic_string);
     if (!json.contains(CONTENTS)) {
         json[CONTENTS] = {};
     }
-    for (auto &content: json[CONTENTS]) {
-        nlohmann::basic_json<>::value_type &basic_json = content["content"];
-        const std::string &content_name = basic_json;
-        // do something...
-        // ik wil een grid systeem bouwen met deze class
+    try {
+        for (nlohmann::basic_json<> &content: json[CONTENTS]) {
+            GridFrame &frame = m_frames[m_frame_count++];
+            frame.setSize(content["size"]["width"], content["size"]["height"]);
+            frame.setGrid(content["grid"]["rows"], content["grid"]["columns"]);
+            for (nlohmann::basic_json<> &component: content[CONTENTS_COMPONENTS]) {
+                const std::string &component_class = component["class"];
+                IPlaceable *placeable = nullptr;
+                if (component_class == "button") {
+                    RoundedButton &button = m_buttons[m_button_count++];
+                    button.setText(component["text"]);
+                    placeable = &button;
+                }
+                if (component_class == "label") {
+                }
+                if (placeable == nullptr) {
+                    std::cout << "Unidentified component class: " << component_class << std::endl;
+                    continue;
+                }
+                const int row = component["grid"]["row"], column = component["grid"]["column"];
+                const int row_span = component["grid"]["row_span"], column_span = component["grid"]["column_span"];
+                frame.addElement(placeable, row - 1, column - 1, row_span, column_span);
+            }
+        }
+    } catch (...) {
+        std::cout << "Failed to load screen configuration." << std::endl;
     }
 }
 
@@ -29,13 +52,43 @@ void ScreenContentManager::save() {
         {
             {"content", "main_menu"},
             {"class", "grid"},
-            {"rows", 4},
-            {"columns", 5},
             {
-                "components", {
+                "size", {
+                    {"width", 1280},
+                    {"height", 800}
+                }
+            },
+            {
+                "grid", {
+                    {"rows", 4},
+                    {"columns", 5},
+                }
+            },
+            {
+                CONTENTS_COMPONENTS, {
                     {
                         {"class", "button"},
-                        {"text", "Restart"}
+                        {"text", "Restart"},
+                        {
+                            "grid", {
+                                {"row", 3},
+                                {"column", 2},
+                                {"row_span", 1},
+                                {"column_span", 1}
+                            }
+                        }
+                    },
+                    {
+                        {"class", "button"},
+                        {"text", "Resurrect"},
+                        {
+                            "grid", {
+                                {"row", 3},
+                                {"column", 2},
+                                {"row_span", 1},
+                                {"column_span", 1}
+                            }
+                        }
                     },
                 }
             },
@@ -47,4 +100,16 @@ void ScreenContentManager::save() {
 
     const nlohmann::json::string_t &basic_string = json.dump(4);
     m_file.write(basic_string.c_str());
+}
+
+void ScreenContentManager::update(const SmartMouse &mouse) {
+    for (int i = 0; i < m_frame_count; i++) {
+        m_frames[i].update(mouse);
+    }
+}
+
+void ScreenContentManager::draw(sf::RenderWindow &window) const {
+    for (int i = 0; i < m_frame_count; i++) {
+        m_frames[i].draw(window);
+    }
 }
--- src/devices/ScreenContentManager.h
+++ src/devices/ScreenContentManager.h
@@ -1,15 +1,30 @@
 #ifndef LEARNINGSFML_SCREENCONTENTMANAGER_H
 #define LEARNINGSFML_SCREENCONTENTMANAGER_H
+
+#include "../gui/GridFrame.h"
+#include "../gui/RoundedButton.h"
+#include "../gui/RoundedStatusBar.h"
 #include "../system/FileHandler.h"
 
 
-class ScreenContentManager {
-public:
+class ScreenContentManager : public IClickable {
     static constexpr char CONTENTS[] = "contents";
     static constexpr char STYLES[] = "styles";
+    static constexpr char CONTENTS_COMPONENTS[] = "components";
+
+protected:
+    static constexpr int FRAMES_MAX = 5;
+    static constexpr int ELEMENTS_MAX = 20;
 
 private:
     FileHandler m_file;
+    std::array<GridFrame, FRAMES_MAX> m_frames;
+    std::array<RoundedButton, ELEMENTS_MAX> m_buttons;
+    std::array<RoundedStatusBar, ELEMENTS_MAX> m_statusbars;
+
+    int m_frame_count = 0;
+    int m_button_count = 0;
+    int m_statusbar_count = 0;
 
 public:
     void setPath(const char *path);
@@ -17,5 +32,9 @@ public:
     void load();
 
     void save();
+
+    void update(const SmartMouse &mouse) override;
+
+    void draw(sf::RenderWindow &window) const override;
 };
 #endif //LEARNINGSFML_SCREENCONTENTMANAGER_H
